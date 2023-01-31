27612468b84e550fabbe3ab900b45b64757f2f3d
Suleyman Baris Eser <suleymanbariseser@gmail.com>
2023-01-31 22:02:59 +0300

    solutions of 3-8-9

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,3 +1,3 @@
-type MyOmit<T, K> = {
+type MyOmit<T, K extends keyof T> = {
   [F in keyof T as F extends K ? never : F]: T[F]
 }
--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,3 @@
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = any> = {
+  readonly [P in keyof T as P extends K ? P : never]: T[P]
+} & Omit<T, K>
